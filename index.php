55b6d3603242e714b312ef199e16577988e95286
Akhmetova <akhmetova1605@gmail.com>
2022-09-28 14:54:12 +0500

    +

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="categories-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><!--?= Html::encode($this->title) ?-->Ntcn</h1>
 
     <p>
         <?= Html::a('Create Categories', ['create'], ['class' => 'btn btn-success']) ?>
